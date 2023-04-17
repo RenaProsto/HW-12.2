6ee39308838e53c485ee236868efa0aa239773db
RenaProsto <renaprosto123@gmail.com>
2023-04-18 02:29:07 +0500

    Готово ДЗ

---
+++ src/Book.java
@@ -0,0 +1,18 @@
+public class Book {
+    String nameBook;
+    int yearBook;
+    public Book(String nameBook, int yearBook) {
+        this.nameBook = nameBook;
+        this.yearBook = yearBook;
+    }
+    public void setYearBook(int yearBook) {
+        this.yearBook = yearBook;
+    }
+    public String getNameBook() {
+        return nameBook;
+    }
+    public int getYearBook() {
+        return yearBook;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author andrzejSapkowski = new Author("Andrzej" , "Sapkowski");
+        Author joanneRowling = new Author("Joanne","Rowling");
+
+        Book theWitcher = new Book("The Witcher" , 1986);
+        Book harryPotter = new Book("Harry Potter and the Philosopher's Stone", 1997);
+
+        System.out.println(theWitcher.getNameBook() + " " + theWitcher.getYearBook()+ " " + andrzejSapkowski.getNameAuthor()+ " " +andrzejSapkowski.getSurnameAuthor());
+        System.out.println(harryPotter.getNameBook()+ " " +harryPotter.getYearBook()+ " " + joanneRowling.getNameAuthor() + " " +joanneRowling.getSurnameAuthor());
+
+        theWitcher.setYearBook(1985);
+        System.out.println(theWitcher.getYearBook() + " Изменен год.");
+
     }
 
 }
