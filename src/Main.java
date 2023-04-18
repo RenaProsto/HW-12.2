8abe0d37b30018d83c237f7ec3bfa43c28136239
RenaProsto <renaprosto123@gmail.com>
2023-04-18 13:15:24 +0500

    Готово ДЗ (ДОРАБОТАЛ добавил в класс Book поле аuthor типа Author + добавил модификаторы доступа.)

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         Author andrzejSapkowski = new Author("Andrzej" , "Sapkowski");
         Author joanneRowling = new Author("Joanne","Rowling");
 
-        Book theWitcher = new Book("The Witcher" , 1986);
-        Book harryPotter = new Book("Harry Potter and the Philosopher's Stone", 1997);
+        Book theWitcher = new Book("The Witcher" ,andrzejSapkowski, 1986);
+        Book harryPotter = new Book("Harry Potter and the Philosopher's Stone",joanneRowling, 1997);
 
         System.out.println(theWitcher.getNameBook() + " " + theWitcher.getYearBook()+ " " + andrzejSapkowski.getNameAuthor()+ " " +andrzejSapkowski.getSurnameAuthor());
         System.out.println(harryPotter.getNameBook()+ " " +harryPotter.getYearBook()+ " " + joanneRowling.getNameAuthor() + " " +joanneRowling.getSurnameAuthor());
